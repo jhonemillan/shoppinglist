2532e4f052082a049dc4b0538b1a0e70708542c0
jhon edwin millan <jhonemillan@gmail.com>
2017-11-15 00:36:04 -0500

    se crea el metodo de cosulta

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
+import { Item } from './../../models/item.model';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular/navigation/ionic-page';
+import { ShoppingProvider } from '../../providers/shopping/shopping.service';
+import { Observable } from 'rxjs/Observable';
+
 
 @IonicPage()
 @Component({
@@ -9,8 +13,14 @@ import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  shoppingList$: Observable <Item[]>;
+
+  constructor(public navCtrl: NavController,
+
+              private service : ShoppingProvider) {
+  this.shoppingList$ = this.service.getShopppigList().snapshotChanges()
+                                   .map(changes => {return changes.map(c=>({key: c.payload.key, ...c.payload.val()}))});
+                      
 
   }
-
 }
