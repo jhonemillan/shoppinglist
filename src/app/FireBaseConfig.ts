b91612de133d471cb677c6b3b4360a40e8d0dbf8
jhonemillan <jhonemillan@gmail.com>
2017-11-17 00:54:25 -0500

    Update FireBaseConfig.ts

--- src/app/FireBaseConfig.ts
+++ src/app/FireBaseConfig.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyCu0w_0dU7_G5HXy5pyrXp0ofNb-Ov2Ljg",
-    authDomain: "shoppinglist-4af3d.firebaseapp.com",
-    databaseURL: "https://shoppinglist-4af3d.firebaseio.com",
-    projectId: "shoppinglist-4af3d",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "shoppinglist-4af3d.appspot.com",
     messagingSenderId: "42990262624"
 }
